651e0240856eda716c70eab710d8f4314594d41b
Irushka1982 <leshchyshyn.iryna@gmail.com>
2015-12-24 16:34:01 +0200

    v rabote

--- GoIT/JS lesson 7-8 v rabote/js/lesson7_8js.js
+++ GoIT/JS lesson 7-8 v rabote/js/lesson7_8js.js
@@ -1,6 +1,6 @@
 
 var mydiv = $('<div/>', {
-    id:     'wrapper',
+    id:     'wrapper'
    });
 $('body').append(mydiv);
 
@@ -55,17 +55,17 @@ $('#content').append(thirdDiv);
 var firstP = $('<p/>',{
 	text:'Proin elit arcu, rutrum commodo, vehicula tempus, commodo a, risus. Curabitur nec arcu. Donec sollicitudin mi sit amet mauris. Nam elementum quam ullamcorper ante. Etiam aliquet massa et lorem. Mauris dapibus lacus auctor risus. Aenean tempor ullamcorper leo. Vivamus sed magna quis ligula eleifend adipiscing' 
 	});
-$('.t1').append(firstP);
+$('div.t1').append(firstP);
 
 var secondP = $('<p/>',{
 	text:'Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, ut pharetra massa metus id nunc. Duis scelerisque molestie turpis. Sed fringilla, massa eget luctus malesuada, metus eros molestie lectus, ut tempus eros massa ut dolor. Aenean aliquet fringilla sem. Suspendisse sed ligula in ligula suscipit aliquam. Praesent in eros vestibulum mi adipiscing adipiscing. Morbi facilisis. Curabitur ornare consequat nunc. Aenean vel metus. Ut posuere viverra nulla. Aliquam erat volutpat. Pellentesque convallis. Maecenas feugiat, tellus pellentesque pretium posuere, felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus'
 });
-$('.t2').append(secondP );
+$('div.t2').append(secondP );
 
 var thirdP = $('<p/>',{
 	text:'Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat port'
 });
-$('.t3').append(thirdP);
+$('div.t3').append(thirdP);
 
 
 
@@ -84,17 +84,7 @@ $j('ul.tabs li').click(function(){
 	$j('div.' + thisClass).show();
 	$j('ul.tabs li').removeClass('tab-current');
 	$j(this).addClass('tab-current');
+	
 	});
-	/*
-$j('ul.tabs li').click(function(){
-	var thisClass = this.className.slice(0,2);
-	$j('div.t11').hide();
-	$j('div.t12').hide();
-	$j('div.t13').hide();
-	$j('div.' + thisClass).show();
-	$j('ul.tabs li').removeClass('tab-current');
-	$j(this).addClass('tab-current');
-	});
-*/
 });
 
