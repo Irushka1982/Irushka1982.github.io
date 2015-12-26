c8ec8fa0d8848290ba0c86412bb414c876adfb6d
Irushka1982 <leshchyshyn.iryna@gmail.com>
2015-12-26 09:54:43 +0200

    na utvergdenie

---
+++ GoIT/JS lesson 7-8_2/js/lesson7-8_2.js
@@ -0,0 +1,114 @@
+var content = $('<div/>', {
+    id:'content',
+   });
+$('body').append(content);
+
+var form = $('<form/>', {
+    html:'<fieldset></fieldset>',
+   });
+$('#content').append(form);
+
+var firstDiv = $('<div/>',{
+    class: 'first',
+});
+$('fieldset').append(firstDiv);
+
+
+var firstLabel = $('<label>')
+.attr('for', 'firstname')
+.text('First name:');
+$('.first').append(firstLabel);
+
+var firstInput = $('<input type="text">')
+.attr({id: 'firstname',
+ name: 'firstname',
+ });
+$('.first').append(firstInput);
+
+var secondDiv = $ ('<div/>',{
+    class: 'second',
+});
+$('fieldset').append(secondDiv);
+
+
+var secondLabel = $('<label/>',{
+    for: 'lastname',
+text: 'Last name:',
+});
+$('.second').append(secondLabel);
+
+
+var secondInput = $('<input/>',{
+    type: 'text',
+id: 'lastname',
+ name: 'lastname',
+  });
+$('.second').append(secondInput);
+
+
+
+var thirdDiv = $ ('<div/>',{
+    class: 'third',
+});
+$('fieldset').append(thirdDiv);
+
+
+var thirdLabel = $('<label/>',{
+    for: 'address',
+text: 'Address:',
+});
+$('.third').append(thirdLabel);
+
+
+var thirdInput = $('<input/>',{
+    type: 'text',
+id: 'address',
+ name: 'address',
+      });
+$('.third').append(thirdInput);
+
+var firstTooltip = $('<p/>',{
+    class: 'firsttooltip',
+    text: 'Please provide your firstname.',
+});
+$('.first').append(firstTooltip);
+
+var secondTooltip = $('<p/>',{
+    class: 'secondtooltip',
+    text: 'Please provide also your lastname.',
+});
+$('.second').append(secondTooltip);
+
+var thirdTooltip = $('<p/>',{
+    class: 'thirdtooltip',
+    text: 'Your home or work address.',
+});
+$('.third').append(thirdTooltip);
+
+var mybutton = $('<button/>',{
+ text: 'Show help',
+click: (function() {
+$('p').animate ({opacity:1})
+})
+}).appendTo('#content');
+
+$('#firstname').mouseover(function() {
+ $('.firsttooltip').animate ({opacity:1})
+});
+$('#firstname').mouseleave(function() {
+ $('.firsttooltip').animate ({opacity:0})
+});
+
+$('#lastname').mouseover(function() {
+ $('.secondtooltip').animate ({opacity:1})
+});
+$('#lastname').mouseleave(function() {
+ $('.secondtooltip').animate ({opacity:0})
+});
+
+$('#address').mouseover(function() {
+ $('.thirdtooltip').animate ({opacity:1})
+});
+$('#address').mouseleave(function() {
+ $('.thirdtooltip').animate ({opacity:0})
+});
