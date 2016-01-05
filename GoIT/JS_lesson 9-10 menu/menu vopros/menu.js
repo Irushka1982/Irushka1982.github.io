17a02a93eb5067326630972157d5d0fed8b0ed56
Irushka1982 <leshchyshyn.iryna@gmail.com>
2016-01-05 20:41:35 +0200

    vopros mouseout

---
+++ GoIT/JS_lesson 9-10 menu/menu vopros/menu.js
@@ -0,0 +1,52 @@
+
+/*
+$(function(){  
+       
+     $('.s').mouseover(function(){  
+           $( '.submenu' ).show();
+           $( '.sub2' ).hide();
+           $( '.sub3' ).hide();
+               });  
+          });  
+
+$(function(){  
+       
+     $('.s').mouseout(function(){  
+              $( '.submenu' ).hide();
+                                    });  
+            });  
+
+*/
+
+
+
+$(function(){  
+       
+     $('.submenu-activ').mouseover(function(){  
+           $( '.sub2' ).show();
+           $( '.sub3' ).hide();
+               });  
+          });  
+
+$(function(){  
+       
+     $('.submenu-activ').mouseout(function(){  
+           $( '.sub2' ).hide();
+                                    });  
+            });  
+
+
+$(function(){  
+       
+     $('.sub2-active').mouseover(function(){  
+               $( '.sub3' ).show();
+                          });  
+            });  
+
+   $(document).ready(function(){  
+       
+     $('.sub2-active').mouseout(function(){  
+           $( '.sub3' ).hide();
+                                    });  
+            });  
+  
