49b0df00c309cfc2bf413ea67fca318bb4e96ac8
Irushka1982 <leshchyshyn.iryna@gmail.com>
2016-01-07 18:22:14 +0200

    vopros border right

--- GoIT/JS/JS_lesson 9-10 v rabote/lesson 9-10 checkbox proba/js/Lesson_9-10.js
+++ GoIT/JS/JS_lesson 9-10 v rabote/lesson 9-10 checkbox proba/js/Lesson_9-10.js
@@ -1,68 +1,11 @@
 
-/*$(".fancybox").fancybox({
-		openEffect	: 'none',
-		closeEffect	: 'none'
-	});*/
+
 jQuery(document).ready(function(){
 
 var params = {
-        changedEl: "#idSelect",
-        visRows: 5,
+        changedEl: ".towns",
+        visRows: 15,
         scrollArrows: true
     }
     cuSel(params);
 });
-/*$(document).ready(function(){  
-  	   
-     $('.submenu3').mouseover(function(){  
-  	    $('.submenu2').stop().animate({opacity: 0})}).mouseout(function(){
-  	     $('.submenu2').stop().animate({opacity: 1})
-
-  	     });  
-  	   
-  	 });  
-$(document).ready(function(){  
-       
-     $(".bla").mouseover(function(){  
-           $( '.submenu2' ).show();
-               });  
-       
-     });  
-$(document).ready(function(){  
-       
-     $(".bla").mouseout(function(){  
-           $( '.submenu2' ).hide();
-               });  
-       
-     });  */
-
-$(function(){  
-       
-     $('.submenu-activ').mouseover(function(){  
-           $( '.sub2' ).show();
-           $( '.sub3' ).hide();
-               });  
-          });  
-
-$(function(){  
-       
-     $('.submenu-activ').mouseout(function(){  
-           $( '.sub2' ).hide();
-                                    });  
-            });  
-
-
-$(function(){  
-       
-     $('.sub2-active').mouseover(function(){  
-               $( '.sub3' ).show();
-                          });  
-            });  
-
-   $(document).ready(function(){  
-       
-     $('.sub2-active').mouseout(function(){  
-           $( '.sub3' ).hide();
-                                    });  
-            });  
-  
