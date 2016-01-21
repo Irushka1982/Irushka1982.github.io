a16478c16407511e5f08c2c1650de7ba02522eab
Irushka1982 <leshchyshyn.iryna@gmail.com>
2016-01-21 11:02:55 +0200

    vopros

--- GoIT/JS/JS lesson 11-12 v rabote/шаблонизатор/js/shablonizator.js
+++ GoIT/JS/JS lesson 11-12 v rabote/шаблонизатор/js/shablonizator.js
@@ -1,25 +1,46 @@
-
-$(function(){
-	var html = $('#test').html();
-	var articles = [
-	{
-		title: 'lesson 1',
-		content: 'lesson 1'
-	},
-	{
-		title: 'lesson 2',
-		content: 'lesson 2'
-	}
-	];
-
-	var content	= tmpl(html, {
-		data: articles
-	});
-
-
-
-	$('body').append(content);
+var content = $('<div/>',{
+	class: 'content',
 });
+$('body').append(content);
+
+var title = $('<h1/>',{
+    class: 'nazva',
+    text: 'Иванов Иван Иванович',
+});
+$('.content').append(title);
+
+var foto = document.createElement("img");
+foto.src = ".//img/IMG_6144.png";
+
+$('.content').append(foto);
+
+
+
+
+
+var who = $('<h3/>',{
+    class: 'who',
+    text: 'Студент заборостроительного техникума',
+});
+$('.content').append(who);
+
+var why = $('<h3/>',{
+    class: 'why',
+    text: 'Хочу учить фронтенд, потому что:',
+});
+$('.content').append(why);
+
+
+
+
+var abc = $('<ul/>',{
+    class: 'abc',
+    
+});
+$('.why').append(abc);
+
+
+
 
 
 
@@ -27,11 +48,20 @@ $(function(){
 	var html = $('#test2').html();
 	var lishka = [
 	{
-		title: 'lesson 1',
+		title: 'Я красивый',
 		
 	},
 	{
-		title: 'lesson 2',
+		title: 'Я умный',
+		
+	},
+	{
+		title: 'Умею вышивать крестиком',
+		
+	},
+
+	{
+		title: 'Умею шить на машинке',
 		
 	}
 	];
@@ -42,5 +72,43 @@ $(function(){
 
 
 
-	$('.blok').append(vstavka);
+	$('.abc').append(vstavka);
 });
+
+
+
+
+
+
+
+
+$(function(){
+	var html = $('#test').html();
+	var about = [
+	{
+		title: 'Мой контактный телефон',
+		content: '+38097222222',
+		more: '+38067222222'
+	},
+	{
+		title: 'мой профиль в соцсетях',
+		content: 'vk.com',
+		more: 'facebook'
+	},
+	{
+		title: 'Мой фидбек',
+		content: 'Если нужно, могу играть на свадьбах',
+		more: 'Если нужно, могу построить вам забор'
+	}
+
+	];
+
+	var content2	= tmpl(html, {
+		data: about
+	});
+
+
+	$('.content').append(content2);
+});
+
+
