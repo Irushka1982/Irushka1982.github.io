bf2819d64ab3de9e5213da0d66ca7b9dea5ab1e0
Irushka1982 <leshchyshyn.iryna@gmail.com>
2016-01-23 19:22:51 +0200

    mistake

---
+++ GoIT/JS/JS lesson 13-14 proba/js/lesson13-14.js
@@ -0,0 +1,30 @@
+$(function(){
+	var html = $('#blok').html();
+	
+	var about = [
+	{
+		"question": "1+2=",
+		"answer":{"value1": 'Мой профиль в соцсетях',
+		"valuevalue2": 'vk.com',
+		"value3": 'facebook'}
+	},
+	{
+		"question": "1+2=",
+		"answer":{"value1": 'Мой профиль в соцсетях',
+		"valuevalue2": 'vk.com',
+		"value3": 'facebook'}
+	},
+	{"question": "1+2=",
+		"answer":{"value1": 'Мой профиль в соцсетях',
+		"valuevalue2": 'vk.com',
+		"value3": 'facebook'}
+	}
+
+	];
+
+var vstavka = tmpl(html, {
+		data: about
+	});
+
+
+('body').append(vstavka);
