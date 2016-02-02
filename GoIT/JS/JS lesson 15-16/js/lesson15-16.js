265e6ecbda1e57cedd916b52eff3a33fc10741cb
Irushka1982 <leshchyshyn.iryna@gmail.com>
2016-02-02 21:46:11 +0200

    vopros

--- GoIT/JS/JS lesson 15-16/js/lesson15-16.js
+++ GoIT/JS/JS lesson 15-16/js/lesson15-16.js
@@ -3,15 +3,9 @@ var pole = document.getElementById('#poletext');
 pole.addEventListener('click', GoogleCallback);
 
 function GoogleCallback (func, zapros) {
-    window[func](zapros);
-}
-
-$.getJSON('http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q='+ 'dance' +'&callback=GoogleCallback&rsz=large&context=?',
-
-function(zapros){
+   
     var div = document.createElement("div")
     div.className = 'main';
-
 
 
     $.each(zapros.results, function(i, val){
@@ -35,4 +29,10 @@ var s = document.createElement("div");
 		        });
 
     $('body').html(div);
-});
+    
+}
+
+$.getJSON('http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q='+ 'dance' +'&callback=GoogleCallback&rsz=large&context=?')
+
+
+
