4461544b47dbfdb755e6846583683cf160bea48f
Irushka1982 <leshchyshyn.iryna@gmail.com>
2016-02-19 14:18:29 +0200

    BEM proverka

---
+++ GoIT/JS/lesson1920/js/lesson19-20.js
@@ -0,0 +1,96 @@
+
+$(document).ready(function() {
+     
+     $('.accordion__title--1').mouseover(function(){  
+      $('.accordion__text--1').show();
+      $('.accordion__text--2').hide();
+      $('.accordion__text--3').hide();
+      $('.accordion__text--4').hide();    
+            
+         });  
+   
+$(function(){  
+            $('.accordion__title--1').mouseout(function(){  
+            $('.accordion__text--1').hide();
+
+                });  
+            });  
+
+
+  $(function(){
+    $('.accordion__title--2').mouseover(function(){  
+      $('.accordion__text--2').show();
+      $('.accordion__text--1').hide();
+      $('.accordion__text--3').hide();
+      $('.accordion__text--4').hide();    
+     });       
+         });  
+   
+$(function(){  
+            $('.accordion__title--2').mouseout(function(){  
+            $('.accordion__text--2').hide();
+                });  
+            });  
+
+
+$(function(){
+    $('.accordion__title--3').mouseover(function(){  
+      $('.accordion__text--3').show();
+      $('.accordion__text--1').hide();
+      $('.accordion__text--2').hide();
+      $('.accordion__text--4').hide();    
+            
+         });  
+  });
+   
+$(function(){  
+            $('.accordion__title--3').mouseout(function(){  
+            $('.accordion__text--3').hide();
+                });  
+            });  
+
+
+
+$(function(){
+    $('.accordion__title--4').mouseover(function(){  
+      $('.accordion__text--4').show();
+      $('.accordion__text--1').hide();
+      $('.accordion__text--2').hide();
+      $('.accordion__text--3').hide();    
+          });  
+         });  
+   
+$(function(){  
+            $('.accordion__title--4').mouseout(function(){  
+            $('.accordion__text--4').hide();
+                });  
+            });  
+
+
+
+
+
+
+});
+
+
+    $(document).ready(function() {
+     
+      $("#owl-demo").owlCarousel({
+     
+          navigation : true, // Show next and prev buttons
+          slideSpeed : 300,
+          paginationSpeed : 400,
+          singleItem:true
+     
+          // "singleItem:true" is a shortcut for:
+          // items : 1, 
+          // itemsDesktop : false,
+          // itemsDesktopSmall : false,
+          // itemsTablet: false,
+          // itemsMobile : false
+     
+      });
+     
+    });
+
