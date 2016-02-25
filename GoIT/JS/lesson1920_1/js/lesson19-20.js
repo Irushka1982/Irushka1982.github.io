d32c0700ec5129714e161f05abffb6eb16c1f8c9
Irushka1982 <leshchyshyn.iryna@gmail.com>
2016-02-25 23:10:18 +0200

    vopros

--- GoIT/JS/lesson1920_1/js/lesson19-20.js
+++ GoIT/JS/lesson1920_1/js/lesson19-20.js
@@ -1,90 +1,13 @@
 
 
   $(document).ready(function(){
+  
    $('.accordion').click(function(){
 $('.accordion').find('.accordion__title').removeClass('active');    
 $('.accordion').find('.accordion__text').hide();    
-
 $(this).find('.accordion__title').addClass('active');
 $(this).find('.accordion__text').show();
-
-
   });
-
-});
-
-
-/*$(document).ready(function() {
-     
-     $('.accordion__title--1').mouseover(function(){  
-      $('.accordion__text--1').show();
-      $('.accordion__text--2').hide();
-      $('.accordion__text--3').hide();
-      $('.accordion__text--4').hide();    
-            
-         });  
-   
-$(function(){  
-            $('.accordion__title--1').mouseout(function(){  
-            $('.accordion__text--1').hide();
-
-                });  
-            });  
-
-
-  $(function(){
-    $('.accordion__title--2').mouseover(function(){  
-      $('.accordion__text--2').show();
-      $('.accordion__text--1').hide();
-      $('.accordion__text--3').hide();
-      $('.accordion__text--4').hide();    
-     });       
-         });  
-   
-$(function(){  
-            $('.accordion__title--2').mouseout(function(){  
-            $('.accordion__text--2').hide();
-                });  
-            });  
-
-
-$(function(){
-    $('.accordion__title--3').mouseover(function(){  
-      $('.accordion__text--3').show();
-      $('.accordion__text--1').hide();
-      $('.accordion__text--2').hide();
-      $('.accordion__text--4').hide();    
-            
-         });  
-  });
-   
-$(function(){  
-            $('.accordion__title--3').mouseout(function(){  
-            $('.accordion__text--3').hide();
-                });  
-            });  
-
-
-
-$(function(){
-    $('.accordion__title--4').mouseover(function(){  
-      $('.accordion__text--4').show();
-      $('.accordion__text--1').hide();
-      $('.accordion__text--2').hide();
-      $('.accordion__text--3').hide();    
-          });  
-         });  
-   
-$(function(){  
-            $('.accordion__title--4').mouseout(function(){  
-            $('.accordion__text--4').hide();
-                });  
-            });  
-
-
-
-
-
 
 });
 
@@ -111,4 +34,3 @@ $(function(){
      
     });
 
-*/
