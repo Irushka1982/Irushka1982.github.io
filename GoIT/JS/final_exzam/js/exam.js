489e19da7889293696179af35c6d7c3f727b4773
Irushka1982 <leshchyshyn.iryna@gmail.com>
2016-03-14 13:09:36 +0200

    zapros

--- GoIT/JS/final_exzam/js/exam.js
+++ GoIT/JS/final_exzam/js/exam.js
@@ -25,8 +25,6 @@ $.getJSON('http://api.pixplorer.co.uk/image?word=cat&amount=5&size=l');
 }); 
 }); 
 
-
-
  function GoogleCallback (func, data) {
 if ($('.main').length === 0){
 	var div = $('<div/>',{
@@ -35,9 +33,8 @@ if ($('.main').length === 0){
     });
     $('#wrapper4').append(div);
 }
-
 $.each(data.results, function(i, val){
-	        
+	       
 	var b = $('<div/>',{
 		class: 'foto',
 		/*html: '<a href="'+val.url+'" title="'+val.url+'" target="_blank">'+val.title+'</a>'*/
