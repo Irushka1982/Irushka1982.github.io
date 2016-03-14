4550005aed3b3ced8bf1d7971e35e33776036f28
Irushka1982 <leshchyshyn.iryna@gmail.com>
2016-03-14 12:47:59 +0200

    zapros vopros

--- GoIT/JS/final_exzam/js/exam.js
+++ GoIT/JS/final_exzam/js/exam.js
@@ -1,8 +1,58 @@
 
-  $(document).ready(function(){
-$('.grid').masonry({
+$(document).ready(function(){
+
+ /*
+$('.grid').isotope({
   // options
   itemSelector: '.grid-item',
-  columnWidth: 200
+  layoutMode: 'fitRows'
 });
+*/
+
+$('html').keydown(function(eventObject){ 
+  if (eventObject.keyCode == 13) { 
+$('.result').empty();
+	/*var name = $('.poletext').val();  */
+$.getJSON('http://api.pixplorer.co.uk/image?word=cat&amount=5&size=l');
+  }
 });
+
+$('#poleotpravka').click(function otpravka(){
+$('.result').empty();
+
+/*	var name = $('.poletext').val();  */
+$.getJSON('http://api.pixplorer.co.uk/image?word=cat&amount=5&size=l');
+}); 
+}); 
+
+
+
+ function GoogleCallback (func, data) {
+if ($('.main').length === 0){
+	var div = $('<div/>',{
+    	class: 'main',
+    	/*html:'<div class="result"></div>'*/
+    });
+    $('#wrapper4').append(div);
+}
+
+$.each(data.results, function(i, val){
+	        
+	var b = $('<div/>',{
+		class: 'foto',
+		/*html: '<a href="'+val.url+'" title="'+val.url+'" target="_blank">'+val.title+'</a>'*/
+	});		                      
+	$('.result').append(b);
+		   
+/*
+	var f = $('<div/>',{
+			class: 'content',
+			html: '<span>'+val.content+'</span>'
+	});
+		$('.result').append(f);*/
+	       });
+
+
+
+};
+
