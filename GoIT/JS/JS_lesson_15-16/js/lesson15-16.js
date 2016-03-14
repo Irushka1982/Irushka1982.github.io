2a797f798d8873af0112c612847132bfc7c644eb
Irushka1982 <leshchyshyn.iryna@gmail.com>
2016-03-14 11:17:09 +0200

    zapros vopros

--- GoIT/JS/JS_lesson_15-16/js/lesson15-16.js
+++ GoIT/JS/JS_lesson_15-16/js/lesson15-16.js
@@ -85,5 +85,5 @@ var newStudent = new Student();
 
 console.log('Student', Student)
 console.log('newStudent', newStudent);
-console.log('newStudent.sex', newStudent.sex)ж
+console.log('newStudent.sex', newStudent.sex);
 
