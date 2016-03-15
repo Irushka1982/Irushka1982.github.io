0611ffc735bca8c1599dad4353069ba71bed65bb
Irushka1982 <leshchyshyn.iryna@gmail.com>
2016-03-15 12:00:36 +0200

    zapros vopros

--- GoIT/JS/final_exzam/js/exam.js
+++ GoIT/JS/final_exzam/js/exam.js
@@ -1,54 +1,60 @@
-/*я пробую прописать єлементарній запрос, чтобы он работал и записывался в штмл*/
-$(document).ready(function(){
-
-	$('#poleotpravka').click(function otpravka(){
-$('.result').empty();
-$.getJSON('http://api.pixplorer.co.uk/image?word=cat&amount=5&size=l', 
-	function(data){
-	console.log(data);
- });
-	});
-
- $(function (func, data) {
-if ($('.main').length === 0){
-	var div = $('<div/>',{
-    	class: 'main',
-    	html:'<div class="result"></div>'
-    });
-    $('#wrapper4').append(div);
-}
-
-$.each(data.results, function(i, val){
-	        
-	var b = $('<div/>',{
-		class: 'grid-item',
-		html: '<div>'+val+'</div>'
-	});		                      
-	$('.result').append(b);
-		   
-
-	var f = $('<div/>',{
-			class: 'grid-item',
-			html: '<div>'+val+'</div>'
-	});
-		$('.result').append(f);
-	       });
-
-});
 
 
+				$(document).ready(function(){
+$('#poleotpravka').click(function otpravka(){
+			/*$('.grid').empty();*/
+			$.getJSON('http://api.pixplorer.co.uk/image?word=cat&amount=5&size=l', 
+				function(data){	console.log(data);
+                      });
+	}); /* этот код возвращает значение Object {images: Array[5], status: "success", count: 5}*/
+		
+				/* вставка полученного запроса в штмл
 
-	$('.grid').masonry({
-	  // options...
-	  itemSelector: '.grid-item',
-	  columnWidth: 150
-	});
+				 $(function (func, data) {
+					if ($('.plitka').length === 0){
+						var div = $('<div/>',{
+					    	class: 'plitka',
+					    	html:'<div class='grid'></div>'
+					    });
+					    $('#wrapper4').append(div);
+					}
+
+						$.each(data.results, function(i, val){
+							        
+							var b = $('<div/>',{
+								class: 'grid-item',
+								html: '<img> +val' //в єтом коде не уверена
+							});		                      
+							$('.grid').append(b);
+								   
+							       });
+
+					});
+
+				 });*/
+
+/*----инициализация плагина для карусели-----*/
+			 $(window).load(function() {
+		         $('.featured').orbit();
+		     });
+
+/*--инициализация masonry---*/
+			$('.grid').masonry({
+				  // options...
+				  itemSelector: '.grid-item',
+				  columnWidth: 150
+				});
+	}); 
 
 
 
-     $(window).load(function() {
-         $('.featured').orbit();
-     });
 
 
-});
+
+
+
+
+    
+
+
+
