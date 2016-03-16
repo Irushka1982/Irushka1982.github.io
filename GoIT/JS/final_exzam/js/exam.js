1e2348bf835f1dc3f38604e3aed0eef339c8be5f
Irushka1982 <leshchyshyn.iryna@gmail.com>
2016-03-16 23:04:00 +0200

    voproz zapros

--- GoIT/JS/final_exzam/js/exam.js
+++ GoIT/JS/final_exzam/js/exam.js
@@ -1,24 +1,71 @@
 
-
-				$(document).ready(function(){
+/*	1)	--разные пробы запроса--
+		$(document).ready(function(){
 $('#search__poleotpravka').click(function otpravka(){
-			/*$('.grid').empty();*/
-			$.getJSON('http://api.pixplorer.co.uk/image?word=cat&amount=5&size=l', 
-				function(data){	console.log(data);
+			jQuery.ajax({
+    type: "POST",
+    url:'http://api.pixplorer.co.uk/image?word=cat&amount=5&size=l',
+    data: ajax_data,
+    cache: false,
+    success: function (html) {
+        if (html.length > 0) {
+            jQuery("#body").append(html);
+        }
+    }
+});console.log(data)
+		});
+});
+*/
+
+
+
+
+/*	2) --разные пробы запроса--
+$(document).ready(function(){                          
+    $('#search__poleotpravka').click(function(){                   
+        $.getJSON('http://api.pixplorer.co.uk/image?word=cat&amount=5&size=l', {}, function(json){  
+            $('#body').append(html);                           
+        });                
+    })
+});*/
+
+
+/* 3) такой код работает :
+$(document).ready(function(){                          
+    $('#search__poleotpravka').click(function(){   
+ 		
+$.ajax({
+        url: 'http://api.pixplorer.co.uk/image?word=black bg&amount=7&size=m',
+        success: function(data){
+            console.log(data)
+        }
+    })
+	});*/	 //этот код возвращается значение Object {images: Array[7], status: "success", count: 7}*/
+
+
+$(document).ready(function(){                          
+    $('#search__poleotpravka').click(function(){    /*$('.grid').empty();*/
+		$.getJSON('http://api.pixplorer.co.uk/image?word=cat&amount=5&size=l', 
+				function(data)
+				{console.log(data);
                       });
-	}); /* этот код возвращает значение Object {images: Array[5], status: "success", count: 5}*/
+	});  /*этот код в консоли отображается значение Object {images: Array[5], status: "success", count: 5}*/
 		
 
 
-				/* -------вставка полученного запроса в штмл-----
+				/* -------вставка полученного запроса в штмл/ Ошибка в консоли Cannot read property 'results' of undefined-----
+				но запрос отрабатывает*/
 
 				 $(function (func, data) {
-					if ($('.plitka').length === 0){
+					if ($('.plitka').length === 0)
+
+					{
 						var div = $('<div/>',{
 					    	class: 'plitka',
-					    	html:'<div class='grid'></div>'
+					    	html:'<div class="grid"></div>'
 					    });
 					    $('#wrapper3').append(div);
+
 					}
 
 						$.each(data.results, function(i, val){
@@ -33,16 +80,15 @@ $('#search__poleotpravka').click(function otpravka(){
 
 					});
 
-				 });*/
+				 
 
 /*----инициализация плагина для карусели-----*/
-			 $(window).load(function() {
-		         $('.featured').orbit();
-		     });
+		 $(window).load(function() {
+		      $('.featured').orbit();
+	   });
 
 /*--инициализация masonry---*/
-			$('.grid').masonry({
-				  // options...
+	$('.grid').masonry({
 				  itemSelector: '.grid-item',
 				  columnWidth: 150
 				});
