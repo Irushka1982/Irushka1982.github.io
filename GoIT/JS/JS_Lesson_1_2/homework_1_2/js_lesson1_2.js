fc0dfe082a41a116987faea833a2f1207cbf8e58
Irushka1982 <leshchyshyn.iryna@gmail.com>
2016-03-30 19:37:14 +0300

    ciclical

--- GoIT/JS/JS_Lesson_1_2/homework_1_2/js_lesson1_2.js
+++ GoIT/JS/JS_Lesson_1_2/homework_1_2/js_lesson1_2.js
@@ -7,12 +7,16 @@ for (var i = 0; i < 5; i++ ) {
 var name = prompt('Введите имя пользователя');
 var isPresentInTheList = false;
 
+
+
     for (var i = 0; i < arr.length; i++){
      if ( arr[i] === name ){ 
-     	isPresentInTheList = true;
-     	break;
+     	isPresentInTheList = true;          
      	} 
+if(name === null) {isPresentInTheList = false};
+     	break;
 }
+
 
 if (isPresentInTheList){
      	alert(name + ',' + ' ' + 'вы успешно вошли');
