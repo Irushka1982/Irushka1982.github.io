746eed424e3ef070390516497e4fe89cf3a0a675
Irushka1982 <leshchyshyn.iryna@gmail.com>
2016-03-31 17:27:19 +0300

    comment

---
+++ GoIT/JS/JS_Lesson_1_2test/homework_1_2/js_lesson1_2.js
@@ -0,0 +1,25 @@
+
+var arr = [];
+for (var i = 0; i < 5; i++ ) {
+	arr[i] = prompt('enter name');
+}
+
+var name = prompt('Введите имя пользователя');
+var isPresentInTheList = false;
+
+
+
+    for (var i = 0; i < arr.length; i++){
+     if ( arr[i] === name ){ 
+     	isPresentInTheList = true;          
+     	} 
+if(name === null) {isPresentInTheList = false};
+     	break;
+}
+
+
+if (isPresentInTheList){
+     	alert(name + ',' + ' ' + 'вы успешно вошли');
+ }else{
+ alert( 'Ошибка' );
+}
---
+++ GoIT/JS/JS_lesson_13-14/JS_lesson_5-6/js/timer.js
@@ -0,0 +1,120 @@
+ var a = document.createElement('div');
+  a.className = 'wrapper';
+   document.body.appendChild(a);
+
+var b = document.createElement('div');
+  b.className = "ciferblat";
+  a.appendChild(b);
+
+var c = document.createElement('div');
+  c.className = "startbutton";
+  a.appendChild(c);
+
+var d = document.createElement('div');
+  d.className = "clearbutton";
+  a.appendChild(d);
+
+var timer = document.createElement('p');
+b.appendChild(timer);
+timer.innerHTML = '00:00:00:000';
+
+
+var start = document.createElement('p');
+c.appendChild(start);
+start.innerHTML = 'Start';
+
+
+
+function startTimer(){
+	start.innerHTML = 'Pause';
+	id = setInterval(function () {
+		timer.innerHTML = calculate();
+}, 1);
+	start.removeEventListener('click', startTimer);
+	start.addEventListener('click', pauseTimer);
+	start.innerHTML = "Pause..";
+}
+
+start.addEventListener('click', startTimer);
+
+
+function pauseTimer(){
+	clearInterval(id);
+	start.addEventListener('click', startTimer);
+	start.innerHTML = 'Cont..';
+}
+
+
+var clear = document.createElement('p');
+d.appendChild(clear);
+clear.innerHTML = 'Clear';
+clear.addEventListener('click', stop);
+
+function stop(){
+
+clearInterval(id);
+timer.innerHTML = '00:00:00:000';
+start.innerHTML = 'Start';
+start.addEventListener('click', startTimer);
+milliseconds = 0;
+seconds = 0;
+minutes = 0;
+hours = 0;
+date = new Date(0, 0);
+}
+
+var date; 
+var milliseconds = 0;
+var seconds = 0;
+var minutes = 0;
+var hours = 0;
+var id;
+var millisecondsTime;
+var date = new Date(0, 0);
+
+function calculate(){
+
+date.setMilliseconds(date.getMilliseconds() + 4);
+	var milliseconds = date.getMilliseconds();
+
+if (milliseconds === 996) {
+	milliseconds = 0;
+	millisecondsTime = "000" ;
+	++seconds;
+}
+if (milliseconds >= 100){
+	millisecondsTime = milliseconds;
+	}
+
+if (milliseconds < 100){
+	millisecondsTime = "0" + Number(milliseconds);
+}
+if (milliseconds < 10){
+	millisecondsTime = "0" + "0" + Number(milliseconds);
+}
+if (seconds < 10){
+	seconds = "0" + Number(seconds);
+}
+if (seconds >= 60) {
+	seconds = 0;
+	++minutes;
+}
+
+if (minutes < 10){
+	minutes = "0" + Number(minutes);
+}
+if (minutes >= 60) {
+	minutes = 0;
+	++hours;
+ }
+
+if (hours < 10){
+	hours = "0" + Number(hours);
+}
+if (hours >= 60) {
+	hours = 0;
+ }
+  ++milliseconds;
+return  hours + ':' + minutes + ':' + seconds + ':' + millisecondsTime;
+
+}
