235dd2777c9c18f08d852f2a096fab454e7b5900
Irushka1982 <leshchyshyn.iryna@gmail.com>
2016-05-01 22:27:44 +0300

    cikle

--- GoIT/JS/JS_Lesson_1_2/homework_1_2/js_lesson1_2.js
+++ GoIT/JS/JS_Lesson_1_2/homework_1_2/js_lesson1_2.js
@@ -2,6 +2,7 @@
 var arr = [];
 for (var i = 0; i < 5; i++ ) {
 	arr[i] = prompt('enter name');
+	if(arr[i] === null) {break};
 }
 
 var name = prompt('Введите имя пользователя');
@@ -12,9 +13,7 @@ var isPresentInTheList = false;
     for (var i = 0; i < arr.length; i++){
      if ( arr[i] === name ){ 
      	isPresentInTheList = true;          
-     	} 
-if(name === null) {break};
-     	
+     	}  
 }
 
 
