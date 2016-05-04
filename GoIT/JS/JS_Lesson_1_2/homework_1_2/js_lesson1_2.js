3c28dac53bce72d5326e2dd410ad71abbe2cb8f9
Irushka1982 <leshchyshyn.iryna@gmail.com>
2016-05-04 14:36:09 +0300

    1_2

--- GoIT/JS/JS_Lesson_1_2/homework_1_2/js_lesson1_2.js
+++ GoIT/JS/JS_Lesson_1_2/homework_1_2/js_lesson1_2.js
@@ -1,12 +1,13 @@
-
+(function(){
 var arr = [];
-
+var flag = false;
 for (var i = 0; i < 5; i++ ) {
 	arr[i] = prompt('enter name');
-	if(arr[i] === null) {break}
-		;
+	if(arr[i] === null) {flag = true;
+		break;}
+	
 }
-
+if (flag === true){return false;}
 var name = prompt('Введите имя пользователя');
 var isPresentInTheList = false;
 
@@ -15,12 +16,16 @@ var isPresentInTheList = false;
     for (var i = 0; i < arr.length; i++){
      if ( arr[i] === name ){ 
      	isPresentInTheList = true;          
-     	}  
+     	} 
+if(name === null) {isPresentInTheList = false;
+	break;}
+     	
 }
-
 
 if (isPresentInTheList){
      	alert(name + ',' + ' ' + 'вы успешно вошли');
  }else{
  alert( 'Ошибка' );
 }
+})
+();
